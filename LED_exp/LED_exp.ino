21a086a1ea09bc06cc3dcefeda676d66909f406c
ccburst <ccburst123@gmail.com>
2020-03-13 18:34:43 +0900

    20200313_a

--- LED_exp/LED_exp.ino
+++ LED_exp/LED_exp.ino
@@ -4,20 +4,16 @@ int now=0;  /*當前亮暗*/
 void setup() {
   pinMode(13,INPUT);
   pinMode(12,OUTPUT);
-  digitalWrite(12,LOW);
+  digitalWrite(12,now);
 }
 
 void loop() {
-  if(digitalRead(13)==HIGH){
-    if(once==0&&now==0){
-      digitalWrite(12,HIGH);
-      now=1;
+  if(digitalRead(13)==1){
+    if(once==0){
+      now=!now;
+      digitalWrite(12,now);
+      once=1;  /*使一次長時按壓僅改變一次亮暗*/
     }
-    else if(once==0&&now==1){
-      digitalWrite(12,LOW);
-      now=0;
-    }
-    once=1;  /*使一次長時按壓僅改變一次亮暗*/
   }
   else{
     once=0;  /*放開按鈕時重設once*/
